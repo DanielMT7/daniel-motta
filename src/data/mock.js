adcc8facaa73cb890da2343619c1f43ab259d37a
Daniel Motta <danielmotta04@hotmail.com>
2024-05-17 17:25:42 -0300

    feature: adiciona mock de dados

---
+++ src/data/mock.js
@@ -0,0 +1,11 @@
+import wotsHome from '../assets/Wots/home.png'
+import wotsHomeMobile from '../assets/Wots/homeMobile.png'
+import wotsClothe from '../assets/Wots/clothe.png'
+import wotsClotheMobile from '../assets/Wots/clotheMobile.png'
+
+export const wotsImages = [
+  wotsHome,
+  wotsClothe,
+  wotsHomeMobile,
+  wotsClotheMobile
+]
