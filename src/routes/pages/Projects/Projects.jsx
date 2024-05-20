7a6a685bf43f471ed86a2de8dad2433228434337
Daniel Motta <danielmotta04@hotmail.com>
2024-05-20 17:06:59 -0300

    refactor: implementa novas funcionalidades na pagina de projetos

--- src/routes/pages/Projects/Projects.jsx
+++ src/routes/pages/Projects/Projects.jsx
@@ -1,9 +1,55 @@
+import useScreenSize from '../../../hooks/useScreenSize'
+
+import Carousel from '../../../components/Carousel/Carousel'
+
+import { wotsInfo, oneRpmInfo } from '../../../data/mock'
+
 import styles from './Projects.module.css'
 
 function Projects() {
+  const isSmallScreen = useScreenSize(760)
+
+  const Project = ({ data }) => {
+    const { title, images, onlyMobileImages, metadata, tags, description } =
+      data
+
+    const imagesBySize = isSmallScreen ? onlyMobileImages : images
+
+    return (
+      <div className={styles.project}>
+        <div className={styles.project_name}>
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+
+        <div className={styles.project_info}>
+          <Carousel data={imagesBySize} metadata={metadata} />
+
+          <div className={styles.tags}>
+            {tags.map((tag, index) => {
+              return <p key={index}>{tag}</p>
+            })}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className={styles.projects_container}>
+      <div className={styles.projects_label}>
+        <h1>Projetos</h1>
+        <p>em desenvolvimento</p>
+      </div>
+
+      <Project data={wotsInfo} />
+
+      <div className={styles.projects_label}>
+        <h1>Projetos</h1>
+        <p>completos</p>
+      </div>
+
+      <Project data={oneRpmInfo} />
     </div>
   )
 }
